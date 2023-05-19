4b77c4dc6c8895b128deaf04970aa1dc68338cac
clovis eyong nkem <cloviseyong@yahoo.com>
2023-05-19 20:42:49 +0700

    init

--- Todo-List-Scripts/Todo-list.js
+++ Todo-List-Scripts/Todo-list.js
@@ -1,4 +1,16 @@
-const todoList =[];
+
+const todoList =['make dinner', 'wash dishes'];
+let todoListHTML = '';
+
+
+for (let i = 0; i < todoList.length; i++){  /* looping through */
+  const todo = todoList[i];   /* variable to store the list */
+  const html =`<p> ${todo}</p>`; /* html to put the list items */
+  todoListHTML  += html;
+}
+console.log(todoListHTML);
+document.querySelector('.js-todo-list')
+.innerHTML = todoListHTML;
 
 function addTodo(){
  const inputElem = document.querySelector('.js-name-input');
